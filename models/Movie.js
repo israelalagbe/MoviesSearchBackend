236d2fccb3ca3ff08ce2ea63560fccee4ccae794
Israel Alagbe <israelalagbe53@gmail.com>
2020-04-25 20:12:26 +0100

    Fixed eslint error

--- models/Movie.js
+++ models/Movie.js
@@ -1,24 +1,24 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var ObjectId = mongoose.Schema.Types.ObjectId
-var MovieSchema = new mongoose.Schema({
-    // email: {
-    //     type: String,
-    //     lowercase: true,
-    //     unique: true,
-    //     required: true
-    // }, 
-    // name:  {
-    //     type: String,
-    //     unique: true,
-    //     required: true
-    // },
-    // user:{
-    //     type:ObjectId,
-    //     ref:'User'
-    // }
+const ObjectId = mongoose.Schema.Types.ObjectId;
+const MovieSchema = new mongoose.Schema({
+  // email: {
+  //     type: String,
+  //     lowercase: true,
+  //     unique: true,
+  //     required: true
+  // },
+  // name:  {
+  //     type: String,
+  //     unique: true,
+  //     required: true
+  // },
+  // user:{
+  //     type:ObjectId,
+  //     ref:'User'
+  // }
 }, {
-    // timestamps: true
-})
+  // timestamps: true
+});
 
 module.exports = mongoose.model('movies', MovieSchema);
