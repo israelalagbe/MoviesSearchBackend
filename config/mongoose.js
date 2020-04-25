b1da9b9c273020dd02979ad38e90a7ab771e86dd
Israel Alagbe <israelalagbe53@gmail.com>
2020-04-25 20:11:28 +0100

    Updated mongoose config

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
-const { env } = require('./helpers/env');
+const { env } = require('../helpers/env');
 
 const mongoose = require('mongoose');
-mongoose.connect(env('DATABASE_URL'), { useNewUrlParser: true });
+mongoose.connect(env('DATABASE_URL'), { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'DB connection error:'));
