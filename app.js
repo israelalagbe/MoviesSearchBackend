338cc0727b1511cf3a8dee77d13a96f6db8fde54
Israel Alagbe <israelalagbe53@gmail.com>
2020-04-26 12:21:30 +0100

    Cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const cors = require('cors');
 require('dotenv').config();
 
 
@@ -14,6 +15,9 @@ const apiRouter = require('./routes/api');
 
 const app = express();
 
+
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
